2ac0431130a5f0283e64ff4bc8f69250f8f78271
keldi <keldijunk@gmail.com>
2019-09-04 23:39:36 -0400

    Runestone revisions
    Everyone always has all 4; adjusting logic

--- DC.js
+++ DC.js
@@ -9,8 +9,7 @@ function funRecalcRunes() {
 	for (var iCtr = 0; iCtr < oSpells.length; iCtr++) {
 		if (oRunes[oSpells[iCtr][0]] == false) {continue;}
 		if (oRunes[oSpells[iCtr][1]] == false) {continue;}
-		if (oRunes[oSpells[iCtr][2]] == false) {continue;}
-		asOut.push(oSpells[iCtr][0] + " " + oSpells[iCtr][1] + " " + oSpells[iCtr][2] + " - " + oSpells[iCtr][3]);
+		asOut.push(oSpells[iCtr][3]);
 	}
 	gel('textOutput').value = asOut.join("\n");
 }
@@ -37,6 +36,7 @@ function funClearRoom() {
 	funDisableRune("A");
 	funDisableRune("B");
 	funDisableRune("C");
+	funDisableRune("D");
 	funDisableRune("F");
 	funDisableRune("G");
 	funDisableRune("H");
@@ -44,6 +44,9 @@ function funClearRoom() {
 	funDisableRune("K");
 	funDisableRune("L");
 	funDisableRune("O");
+	funDisableRune("P");
+	funDisableRune("S");
+	funDisableRune("T");
 	funDisableRune("W");
 	funRecalcRunes();
 }
@@ -56,6 +59,7 @@ function funInitialize() {
 		"A": false,
 		"B": false,
 		"C": false,
+		"D": false,
 		"F": false,
 		"G": false,
 		"H": false,
@@ -63,34 +67,60 @@ function funInitialize() {
 		"K": false,
 		"L": false,
 		"O": false,
-		"W": false,
+		"P": false,
 		"S": false,
 		"T": false,
-		"D": false,
-		"E": false
+		"W": false
 	};
 
+	/*
 	oSpells = [];
-	oSpells.push(["L", "F", "S", "Life From Stone"]);
-	oSpells.push(["S", "F", "C", "Solomon's Flying Carpet"]);
-	oSpells.push(["T", "S", "K", "The Skeleton Key"]);
-	oSpells.push(["D", "B", "G", "Dead Be Gone"]);
-	oSpells.push(["T", "H", "F", "Treasure Hunter's Friend"]);
-	oSpells.push(["D", "S", "B", "Double Score Bonus"]);
-	oSpells.push(["P", "A", "D", "Potency and Durability"]);
-	oSpells.push(["T", "P", "I", "The Phoenix Incantation"]);
-	oSpells.push(["S", "O", "D", "Sigil Of Death"]);
-	oSpells.push(["T", "E", "B", "Tri Elemental Blast"]);
-	oSpells.push(["E", "C", "G", "Enchanted Coin Geyser"]);
-	oSpells.push(["E", "I", "S", "Extra Item Stock"]);
-	//oSpells.push(["T", "T", "F", "The True Fist"]);
-	oSpells.push(["C", "S", "W", "Call Sylphide's Wind"]);
-	//oSpells.push(["S", "B", "S", "Strength Beyond Strength"]);
-	oSpells.push(["P", "T", "F", "Petrify The Flesh"]);
-	oSpells.push(["O", "T", "S", "Open the Sesame"]);
-	oSpells.push(["D", "I", "E", "D I E"]);
-	oSpells.push(["S", "O", "L", "Salve of Life"]);
-	oSpells.push(["G", "W", "S", "Generate Weapon Stash"]);
+	oSpells.push(["L", "F", "S", "L F S - Life From Stone"]);
+	oSpells.push(["S", "F", "C", "S F C - Solomon's Flying Carpet"]);
+	oSpells.push(["T", "S", "K", "T S K - The Skeleton Key"]);
+	oSpells.push(["D", "B", "G", "D B G - Dead Be Gone"]);
+	oSpells.push(["T", "H", "F", "T H F - Treasure Hunter's Friend"]);
+	oSpells.push(["D", "S", "B", "D S B - Double Score Bonus"]);
+	oSpells.push(["P", "A", "D", "P A D - Potency and Durability"]);
+	oSpells.push(["T", "P", "I", "T P I - The Phoenix Incantation"]);
+	oSpells.push(["S", "O", "D", "S O D - Sigil Of Death"]);
+	oSpells.push(["T", "E", "B", "T E B - Tri Elemental Blast"]);
+	oSpells.push(["E", "C", "G", "E C G - Enchanted Coin Geyser"]);
+	oSpells.push(["E", "I", "S", "E I S - Extra Item Stock"]);
+	oSpells.push(["T", "T", "F", "T T F - The True Fist"]);
+	oSpells.push(["C", "S", "W", "C S W - Call Sylphide's Wind"]);
+	oSpells.push(["S", "B", "S", "S B S - Strength Beyond Strength"]);
+	oSpells.push(["P", "T", "F", "P T F - Petrify The Flesh"]);
+	oSpells.push(["O", "T", "S", "O T S - Open the Sesame"]);
+	oSpells.push(["D", "I", "E", "D I E - DIE"]);
+	oSpells.push(["S", "O", "L", "S O L - Salve of Life"]);
+	oSpells.push(["G", "W", "S", "G W S - Generate Weapon Stash"]);
+	*/
+
+
+	//Assuming player always has all 4 rune stones, made revisions.
+	oSpells = [];
+	oSpells.push(["L", "F", "L F S - Life From Stone"]);
+	oSpells.push(["F", "C", "S F C - Solomon's Flying Carpet"]);
+	oSpells.push(["K", "K", "T S K - The Skeleton Key"]);
+	oSpells.push(["B", "G", "D B G - Dead Be Gone"]);
+	oSpells.push(["H", "F", "T H F - Treasure Hunter's Friend"]);
+	oSpells.push(["B", "B", "D S B - Double Score Bonus"]);
+	oSpells.push(["P", "A", "P A D - Potency and Durability"]);
+	oSpells.push(["P", "I", "T P I - The Phoenix Incantation"]);
+	oSpells.push(["O", "O", "S O D - Sigil Of Death"]);
+	oSpells.push(["B", "B", "T E B - Tri Elemental Blast"]);
+	oSpells.push(["C", "G", "E C G - Enchanted Coin Geyser"]);
+	oSpells.push(["I", "I", "E I S - Extra Item Stock"]);
+	oSpells.push(["T", "F", "T T F - The True Fist"]);
+	oSpells.push(["C", "W", "C S W - Call Sylphide's Wind"]);
+	oSpells.push(["B", "S", "S B S - Strength Beyond Strength"]);
+	oSpells.push(["P", "F", "P T F - Petrify The Flesh"]);
+	oSpells.push(["O", "O", "O T S - Open the Sesame"]);
+	oSpells.push(["I", "I", "D I E - DIE"]);
+	oSpells.push(["O", "L", "S O L - Salve of Life"]);
+	oSpells.push(["G", "W", "G W S - Generate Weapon Stash"]);
+
 }
 
 function gel(vsId) {return document.getElementById(vsId);}
